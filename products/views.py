146916cf1b2aafa83df026badbb00ce6966b031b
EirEoin <eoinc2010@live.ie>
2022-11-15 16:28:07 +0000

    trying to fix template does not exist by adding home folder

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_produts(request):
+    """ A view to show all products, including sorting and searching """
+
+    return render(request, 'products/products.html', context)
