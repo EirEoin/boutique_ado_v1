11186aae8ecc52f448f4d53293f63e7874d256b1
EirEoin <eoinc2010@live.ie>
2022-11-17 17:41:36 +0000

    added items to shopping bag

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -21,7 +23,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -53,13 +56,14 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-def product_detail(request, product_id):
-    """ A view to show individual products """
 
-    products = get_object_or_404(Product, pk=product_id)
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': product,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
